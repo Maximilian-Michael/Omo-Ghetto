03e7188ca5bf082470cb1850483f0eb9e450fcea
Oreoluwa32 <adewalepete08@gmail.com>
2022-11-22 22:48:14 +0100

    Added character and animations
    Character and Animation

---
+++ Omo Ghetto/Assets/Scripts/MusicController.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MusicController : MonoBehaviour
+{
+    public static MusicController instance;
+
+    private void Awake()
+    {
+        DontDestroyOnLoad(this.gameObject);
+        if(instance == null)
+        {
+            instance = this;
+        }
+        else
+        {
+            Destroy(gameObject);
+        }
+    }
+}
